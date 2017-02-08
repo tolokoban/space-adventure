eafa98e3cafc587b0fb34d6b2080a5f3338d1aa6
Tolokoban <contact@tolokoban.org>
2017-02-08 08:55:30 +0100

    Update offline.js

--- offline.js
+++ offline.js
@@ -1,53 +1,53 @@
 var CACHE = 'cache-and-update';
 
 self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-  evt.waitUntil(precache());
+    console.log('The service worker is being installed.');
+    evt.waitUntil(precache());
 });
 
 self.addEventListener('activate', function(evt) {
-  console.log('The service worker is being activated.');
-  evt.waitUntil(precache());
+    console.log('The service worker is being activated.');
+    evt.waitUntil(precache());
 });
 
 self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.', evt);
-  evt.respondWith(fromCache(evt.request));
-  evt.waitUntil(update(evt.request));
+    console.log('The service worker is serving the asset.');
+    console.log(evt.request.url);
+    evt.respondWith(fromCache(evt.request));
+    evt.waitUntil(update(evt.request));
 });
 
 function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll([
-      './index.html',
-      './manifest.json',
-      './js/@index.js',
-      './css/@index.css',
-      './css/app/earth.mini.png',
-      './css/app/earth.png',
-      './css/app/hero.mini.png',
-      './css/app/hero.png',
-      './css/app/icon-192.png',
-      './css/app/icon-512.png',
-      './css/app/moon.mini.png',
-      './css/app/moon.png'
-    ]);
-  });
+    return caches.open(CACHE).then(function (cache) {
+        return cache.addAll([
+            './index.html',
+            './manifest.json',
+            './js/@index.js',
+            './css/@index.css',
+            './css/app/earth.mini.png',
+            './css/app/earth.png',
+            './css/app/hero.mini.png',
+            './css/app/hero.png',
+            './css/app/icon-192.png',
+            './css/app/icon-512.png',
+            './css/app/moon.mini.png',
+            './css/app/moon.png'
+        ]);
+    });
 }
 
 function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
+    return caches.open(CACHE).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            return matching || Promise.reject('no-match');
+        });
     });
-  });
 }
 
 function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
+    return caches.open(CACHE).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response);
+        });
     });
-  });
 }
-
